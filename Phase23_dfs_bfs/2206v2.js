ef6d57e5d1939cc99343e8912970d95b9b85bfdf
HyunSJ <dudtod1596@gmail.com>
2022-02-07 20:37:25 +0900

    bfs 2206

--- Phase23_dfs_bfs/2206v2.js
+++ Phase23_dfs_bfs/2206v2.js
@@ -8,60 +8,21 @@ const [nm, ...inputs] = (
 // `6 4\n0000\n1110\n1000\n0010\n0111\n0100`
 const [N, M] = nm.split(" ").map(Number);
 const MAP = inputs.map((e) => e.split("").map(Number));
-const visit = Array.from(Array(N), () => Array.from(Array(M), () => new Array(2).fill(0)));
+const visit = Array.from(Array(2), () => Array.from(Array(N), () => new Array(M).fill(false)));
+console.log(visit);
 const dx = [-1, 0, 1, 0];
 const dy = [0, 1, 0, -1];
-
-class node {
-  value;
-  next;
-  constructor(val) {
-    this.value = val;
-    this.next = null;
-  }
-}
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-  empty() {
-    return this.size === 0 ? true : false;
-  }
-  push(value) {
-    const newNode = new node(value);
-    if (this.head === null) {
-      this.head = newNode;
-      this.head.next = null;
-    } else {
-      this.tail.next = newNode;
-    }
-    this.tail = newNode;
-    this.size++;
-  }
-  pop() {
-    if (this.empty()) return;
-    const node = this.head;
-    this.head = this.head.next;
-    this.size--;
-    return node.value;
-  }
-}
 
 const checkBound = (x, y) => {
   return x >= 0 && x < N && y >= 0 && y < M ? true : false;
 };
 const bfs = (x, y, z) => {
   visit[x][y][z] = 1;
-  //   const queue = [];
-  const queue = new Queue();
+  const queue = [];
   queue.push([x, y, z]);
 
-  //   while (queue.length) {
-  while (!queue.empty()) {
-    // let [qx, qy, qz] = queue.shift();
-    let [qx, qy, qz] = queue.pop();
+  while (queue.length) {
+    let [qx, qy, qz] = queue.shift();
 
     if (qx === N - 1 && qy === M - 1) {
       console.log(visit[qx][qy][qz]);
